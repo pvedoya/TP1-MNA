af67e6f94a78c12c0e90328ed1a8ed3b5e82e5d8
Santiago Reyes <sreyes@itba.edu.ar>
2020-10-07 18:53:33 -0300

    mean weight aprox implementation and test notes

--- main.py
+++ main.py
@@ -52,8 +52,44 @@ else:
 # Returns array of match percentages for each person
 match_percentages = calculate_match_percentages(weights, people_groups, anon_weight)
 sorted_indexes = np.argsort(match_percentages)
+# group = matched_groups[0]
+match_dict = {}
+for g in sorted_indexes:
+    group_eigenvalues = weights[g*per_person_amount:g*per_person_amount+per_person_amount, :]
+    mean_group_eigenvalues = np.mean(group_eigenvalues, 0).reshape(((weights.shape[1],1)))
+    # mean_group_eigenvalues = group_eigenvalues[2, :].reshape(((175,1)))
+    # print(mean_group_eigenvalues.shape)
+    # print(anon_weight.shape)
 
-print('Match percentages:')
+    p = np.absolute(np.dot(anon_weight, mean_group_eigenvalues))/(np.linalg.norm(anon_weight, 2) * np.linalg.norm(mean_group_eigenvalues, 2))
+    match_dict[people_dict[g+1]] = (p[0][0]*100)
+    # print(f"Person: {people_dict[g+1]} match similarity: {p[0][0] * 100}%")
+"""
+Agarrar todos las pelotitas zules (es decir el grupo con el que matcheo)
+mean([v1, v2, v3, v4, ...]) = [v_mean]  -> vector promedio del grupo con el que matcheo
+anon_weight -> vector que estoy evaluando
+
+para ver el porcentaje de cercania:
+    p = v_mean dot anon_weight ->  un numero que esta entre 0 y norm(v_mean)*norm(anon_weight) -> por la desigualdad de cauchy shwartz
+    
+    p/(norm(v_mean) * norm(anon_weight)) --> un numero entro 0 y 1
+
+    0: quiere decir que son ortogonales osea que son nada que ver
+    1: quiere decir que son el mismo
+
+    por lo tanto, podemos decir que p es el parecido entre una persona y otra
+
+
+    if p < threshold:
+        print('no esta en el set')
+
+"""
+
+for k,v in sorted(match_dict.items(), key=lambda x: x[1], reverse=True):
+    print("%-20s %5.2f%%"%(f"Person: {k}", v))
+
+print('SVM Match:')
 for idx in sorted_indexes:
     percentage = match_percentages[idx]
-    print(f'Person: {people_dict[idx+1]} - match: {match_percentages[idx]*100}%')
+    if match_percentages[idx] == 1:
+        print(f'Person: {people_dict[idx+1]} - match: {match_percentages[idx]*100}%')
