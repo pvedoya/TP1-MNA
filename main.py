e2595ea028bdba1a2d38a76d959cff4a2389d39d
mtarradellas <mtarradellas@itba.edu.ar>
2020-10-08 15:42:05 -0300

    svm now only returns prediction instead of array

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils import find_closest_match
 from utils import generate_photo_matrix
 from utils import generate_photo_vector
 from utils import generate_face
-from utils import calculate_match_percentages
+from utils import calculate_match
 from KPCA import KPCA
 from PCA import PCA
 from face_detection import face_recognition
@@ -49,47 +49,18 @@ if is_kpca:
 else:
     weights, anon_weight = PCA(photo_matrix, anon_vector, eigenvector_amount)
 
-# Returns array of match percentages for each person
-match_percentages = calculate_match_percentages(weights, people_groups, anon_weight)
-sorted_indexes = np.argsort(match_percentages)
-# group = matched_groups[0]
+# Returns id of matched person
+match_id = calculate_match(weights, people_groups, anon_weight)
+group_id_list = list(map(int, dict.fromkeys(people_groups)))
 match_dict = {}
-for g in sorted_indexes:
-    group_eigenvalues = weights[g*per_person_amount:g*per_person_amount+per_person_amount, :]
+for g in group_id_list:
+    i = g-1
+    group_eigenvalues = weights[i*per_person_amount:i*per_person_amount+per_person_amount, :]
     mean_group_eigenvalues = np.mean(group_eigenvalues, 0).reshape(((weights.shape[1],1)))
-    # mean_group_eigenvalues = group_eigenvalues[2, :].reshape(((175,1)))
-    # print(mean_group_eigenvalues.shape)
-    # print(anon_weight.shape)
-
     p = np.absolute(np.dot(anon_weight, mean_group_eigenvalues))/(np.linalg.norm(anon_weight, 2) * np.linalg.norm(mean_group_eigenvalues, 2))
-    match_dict[people_dict[g+1]] = (p[0][0]*100)
-    # print(f"Person: {people_dict[g+1]} match similarity: {p[0][0] * 100}%")
-"""
-Agarrar todos las pelotitas zules (es decir el grupo con el que matcheo)
-mean([v1, v2, v3, v4, ...]) = [v_mean]  -> vector promedio del grupo con el que matcheo
-anon_weight -> vector que estoy evaluando
-
-para ver el porcentaje de cercania:
-    p = v_mean dot anon_weight ->  un numero que esta entre 0 y norm(v_mean)*norm(anon_weight) -> por la desigualdad de cauchy shwartz
-    
-    p/(norm(v_mean) * norm(anon_weight)) --> un numero entro 0 y 1
-
-    0: quiere decir que son ortogonales osea que son nada que ver
-    1: quiere decir que son el mismo
-
-    por lo tanto, podemos decir que p es el parecido entre una persona y otra
-
-
-    if p < threshold:
-        print('no esta en el set')
-
-"""
+    match_dict[people_dict[g]] = (p[0][0]*100)
 
 for k,v in sorted(match_dict.items(), key=lambda x: x[1], reverse=True):
     print("%-20s %5.2f%%"%(f"Person: {k}", v))
 
-print('SVM Match:')
-for idx in sorted_indexes:
-    percentage = match_percentages[idx]
-    if match_percentages[idx] == 1:
-        print(f'Person: {people_dict[idx+1]} - match: {match_percentages[idx]*100}%')
+print(f'SVM Match: {people_dict[match_id]}')
